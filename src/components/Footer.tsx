8fcbd485da3fc3496233a9874173838d6e3c317e
PabloSzx <pablosaez1995@gmail.com>
2020-04-28 11:37:48 -0400

    fix footer size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,8 +14,14 @@ export const Footer: FC = () => {
         justifyContent="center"
         paddingTop="20px"
         paddingBottom="20px"
+        maxHeight="150px"
       >
-        <Image maxWidth="400px" alt="logo_uach" src="/logo_uach.jpg" />
+        <Image
+          objectFit="contain"
+          maxWidth="400px"
+          alt="logo_uach"
+          src="/logo_uach.jpg"
+        />
       </Flex>
     </>
   );
